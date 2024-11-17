4d97a92f0896035fc2c92299e4ac8ea9d056524c
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-11-16 21:36:08 -0500

    button component added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants";
 
 function Hero() {
-  return <div></div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 border-2 max-container border-red-500 p-2 "
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike </span>Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals,quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Shop now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat, index) => (
+            <div key={index}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
