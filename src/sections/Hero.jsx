cde4043f943a4af198be57198af8499b84bf7a90
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-11-17 20:59:14 -0500

    update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,9 +10,11 @@ function Hero() {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 border-2 max-container border-red-500 p-2 "
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className="text:xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[83px] font-bold">
+          <span className="xl">The New Arrival</span>
           <br />
           <span>Nike </span>Shoes
         </h1>
